25b306c4217844ed38bed1490e2126a4809427d5
t-wallet <tw.personal@tutanota.com>
2024-07-24 10:46:58 +0200

    Add CI workflow

--- .ci/test_cabal.sh
+++ .ci/test_cabal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xeou pipefail
 
-cabal build all --constraint=clash-prelude==$clash_version  -fci
-cabal run unittests --constraint=clash-prelude==$clash_version -fci --enable-tests
-cabal run doctests --constraint=clash-prelude==$clash_version -fci --enable-tests
-cabal sdist
+cabal v2-build all -fci
+cabal v2-run unittests -fci --enable-tests
+cabal v2-run doctests -fci --enable-tests
+cabal v2-sdist
--- .ci/test_stack.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xeou pipefail
-
-stack --version
-stack build
-stack test
