clash-lang/clash-compiler

Add xpmCdcSyncRst

tests/shouldwork/Cores/Xilinx/XpmCdcSyncRst.hs
@@ -1,0 +1,70 @@
+module XpmCdcSyncRst where
+
+import Clash.Explicit.Prelude
+
+import Data.Proxy
+
+import XpmTestCommon (D3, D5, D10, D11)
+
+import qualified XpmCdcSyncRstTypes as Types
+
+-- | This 'topEntity' exists to make @clash-testsuite@ happy. Without it cannot
+-- find the test benches.
+topEntity :: Unsigned 1
+topEntity = 0
+
+tb0 = done
+ where
+  --                          src dst stages  samples              init
+  done =             Types.tb @D3 @D5 @4      @100     Proxy Proxy Nothing SNat expected
+  expected = $(Types.expected @D3 @D5 @4      @100     Proxy Proxy Nothing SNat SNat)
+{-# ANN tb0 (TestBench 'topEntity) #-}
+
+tb1 = done
+ where
+  --                          src dst stages  samples              init
+  done =             Types.tb @D5 @D3 @4      @100     Proxy Proxy Nothing SNat expected
+  expected = $(Types.expected @D5 @D3 @4      @100     Proxy Proxy Nothing SNat SNat)
+{-# ANN tb1 (TestBench 'topEntity) #-}
+
+tb2 = done
+ where
+  --                          src dst stages  samples              init
+  done =             Types.tb @D3 @D5 @10     @100     Proxy Proxy (Just True) SNat expected
+  expected = $(Types.expected @D3 @D5 @10     @100     Proxy Proxy (Just True) SNat SNat)
+{-# ANN tb2 (TestBench 'topEntity) #-}
+
+tb3 = done
+ where
+  --                          src dst stages  samples              init
+  done =             Types.tb @D3 @D5 @2      @100     Proxy Proxy (Just False) SNat expected
+  expected = $(Types.expected @D3 @D5 @2      @100     Proxy Proxy (Just False) SNat SNat)
+{-# ANN tb3 (TestBench 'topEntity) #-}
+
+tb4 = done
+ where
+  --                          src dst  stages  samples              init
+  done =             Types.tb @D5 @D10 @2      @100     Proxy Proxy Nothing SNat expected
+  expected = $(Types.expected @D5 @D10 @2      @100     Proxy Proxy Nothing SNat SNat)
+{-# ANN tb4 (TestBench 'topEntity) #-}
+
+tb5 = done
+ where
+  --                          src dst  stages  samples              init
+  done =             Types.tb @D10 @D5 @2      @100     Proxy Proxy Nothing SNat expected
+  expected = $(Types.expected @D10 @D5 @2      @100     Proxy Proxy Nothing SNat SNat)
+{-# ANN tb5 (TestBench 'topEntity) #-}
+
+tb6 = done
+ where
+  --                          src dst  stages  samples              init
+  done =             Types.tb @D5 @D11 @2      @100     Proxy Proxy (Just True) SNat expected
+  expected = $(Types.expected @D5 @D11 @2      @100     Proxy Proxy (Just True) SNat SNat)
+{-# ANN tb6 (TestBench 'topEntity) #-}
+
+tb7 = done
+ where
+  --                          src dst  stages  samples              init
+  done =             Types.tb @D11 @D5 @2      @100     Proxy Proxy (Just False) SNat expected
+  expected = $(Types.expected @D11 @D5 @2      @100     Proxy Proxy (Just False) SNat SNat)
+{-# ANN tb7 (TestBench 'topEntity) #-}

tests/shouldwork/Cores/Xilinx/XpmCdcSyncRstTypes.hs
@@ -1,0 +1,79 @@
+{-# LANGUAGE BangPatterns #-}
+
+module XpmCdcSyncRstTypes where
+
+import Clash.Explicit.Prelude
+import Clash.Explicit.Testbench
+
+import Data.Proxy
+import Language.Haskell.TH.Lib
+
+import Clash.Cores.Xilinx.Xpm.Cdc.SyncRst
+import XpmTestCommon
+
+randomRstSrc :: KnownDomain dom => Clock dom -> Reset dom
+randomRstSrc clk = unsafeFromActiveHigh $ genTestData randomSeed clk
+
+tb ::
+  forall a b stages n .
+  ( KnownNat n, 1 <= n
+  , KnownNat stages, 2 <= stages, stages <= 10
+  , KnownDomain a
+  , KnownDomain b
+  ) =>
+  Proxy a -> Proxy b ->
+  -- | Initial values
+  Maybe Bool ->
+  SNat stages ->
+  -- | Expected data
+  Vec n (BitVector 1) ->
+  Signal b Bool
+tb Proxy Proxy initVals SNat expectedDat = done
+ where
+  actual =
+    xpmCdcSyncRstWith
+      @stages
+      (XpmCdcSyncRstConfig (SNat @stages) initVals)
+      clkA clkB (randomRstSrc clkA)
+
+  done =
+    outputVerifierWith
+      (\clk rst -> assertBitVector clk rst $(lift $ "outputVerifier (seed:" <> show randomSeed <> ")"))
+      clkB clkB noReset
+      expectedDat
+      (pack <$> unsafeToActiveHigh actual)
+  rstA = randomRstSrc clkA
+  rstB = unsafeFromActiveHigh $ unsafeSynchronizer clkA clkB $ unsafeToActiveHigh rstA
+
+  -- Testbench clocks
+  clkA :: Clock a
+  clkA = tbClockGen (not <$> unsafeSynchronizer clkB clkA done)
+  clkB :: Clock b
+  clkB = tbClockGen (not <$> done)
+
+expected ::
+  forall a b stages samples .
+  ( KnownDomain a
+  , KnownDomain b
+  , 2 <= stages, stages <= 10
+  ) =>
+  Proxy a ->
+  Proxy b ->
+  -- | Initial values
+  Maybe Bool ->
+  SNat stages ->
+  SNat samples ->
+  ExpQ
+expected Proxy Proxy initVals SNat SNat = listToVecTH out1
+ where
+  out0 = unsafeToActiveHigh $
+    xpmCdcSyncRstWith
+      @stages
+      (XpmCdcSyncRstConfig (SNat @stages) initVals)
+      clkA
+      clkB
+      (randomRstSrc clkA)
+  clkA = clockGen @a
+  clkB = clockGen @b
+
+  out1 = pack <$> sampleN (natToNum @samples) out0

clash-lang/clash-cores

XpmCdcSyncRst.hs
@@ -1,0 +1,71 @@
+module XpmCdcSyncRst where
+
+import Clash.Explicit.Prelude
+
+import Data.Proxy
+
+import XpmTestCommon (D3, D5, D10, D11)
+
+import Clash.Cores.Xilinx.Xpm.Cdc.SyncRst (Asserted(..))
+import qualified XpmCdcSyncRstTypes as Types
+
+-- | This 'topEntity' exists to make @clash-testsuite@ happy. Without it cannot
+-- find the test benches.
+topEntity :: Unsigned 1
+topEntity = 0
+
+tb0 = done
+ where
+  --                          src dst stages  samples              init
+  done =             Types.tb @D3 @D5 @4      @100     Proxy Proxy Nothing SNat expected
+  expected = $(Types.expected @D3 @D5 @4      @100     Proxy Proxy Nothing SNat SNat)
+{-# ANN tb0 (TestBench 'topEntity) #-}
+
+tb1 = done
+ where
+  --                          src dst stages  samples              init
+  done =             Types.tb @D5 @D3 @4      @100     Proxy Proxy Nothing SNat expected
+  expected = $(Types.expected @D5 @D3 @4      @100     Proxy Proxy Nothing SNat SNat)
+{-# ANN tb1 (TestBench 'topEntity) #-}
+
+tb2 = done
+ where
+  --                          src dst stages  samples              init
+  done =             Types.tb @D3 @D5 @10     @100     Proxy Proxy (Just Asserted) SNat expected
+  expected = $(Types.expected @D3 @D5 @10     @100     Proxy Proxy (Just Asserted) SNat SNat)
+{-# ANN tb2 (TestBench 'topEntity) #-}
+
+tb3 = done
+ where
+  --                          src dst stages  samples              init
+  done =             Types.tb @D3 @D5 @2      @100     Proxy Proxy (Just Deasserted) SNat expected
+  expected = $(Types.expected @D3 @D5 @2      @100     Proxy Proxy (Just Deasserted) SNat SNat)
+{-# ANN tb3 (TestBench 'topEntity) #-}
+
+tb4 = done
+ where
+  --                          src dst  stages  samples              init
+  done =             Types.tb @D5 @D10 @2      @100     Proxy Proxy Nothing SNat expected
+  expected = $(Types.expected @D5 @D10 @2      @100     Proxy Proxy Nothing SNat SNat)
+{-# ANN tb4 (TestBench 'topEntity) #-}
+
+tb5 = done
+ where
+  --                          src dst  stages  samples              init
+  done =             Types.tb @D10 @D5 @2      @100     Proxy Proxy Nothing SNat expected
+  expected = $(Types.expected @D10 @D5 @2      @100     Proxy Proxy Nothing SNat SNat)
+{-# ANN tb5 (TestBench 'topEntity) #-}
+
+tb6 = done
+ where
+  --                          src dst  stages  samples              init
+  done =             Types.tb @D5 @D11 @2      @100     Proxy Proxy (Just Asserted) SNat expected
+  expected = $(Types.expected @D5 @D11 @2      @100     Proxy Proxy (Just Asserted) SNat SNat)
+{-# ANN tb6 (TestBench 'topEntity) #-}
+
+tb7 = done
+ where
+  --                          src dst  stages  samples              init
+  done =             Types.tb @D11 @D5 @2      @100     Proxy Proxy (Just Deasserted) SNat expected
+  expected = $(Types.expected @D11 @D5 @2      @100     Proxy Proxy (Just Deasserted) SNat SNat)
+{-# ANN tb7 (TestBench 'topEntity) #-}

XpmCdcSyncRstTypes.hs
@@ -1,0 +1,79 @@
+{-# LANGUAGE BangPatterns #-}
+
+module XpmCdcSyncRstTypes where
+
+import Clash.Explicit.Prelude
+import Clash.Explicit.Testbench
+
+import Data.Proxy
+import Language.Haskell.TH.Lib
+
+import Clash.Cores.Xilinx.Xpm.Cdc.SyncRst
+import XpmTestCommon
+
+randomRstSrc :: KnownDomain dom => Clock dom -> Reset dom
+randomRstSrc clk = unsafeFromActiveHigh $ genTestData randomSeed clk
+
+tb ::
+  forall a b stages n .
+  ( KnownNat n, 1 <= n
+  , KnownNat stages, 2 <= stages, stages <= 10
+  , KnownDomain a
+  , KnownDomain b
+  ) =>
+  Proxy a -> Proxy b ->
+  -- | Initial values
+  Maybe Asserted ->
+  SNat stages ->
+  -- | Expected data
+  Vec n (BitVector 1) ->
+  Signal b Bool
+tb Proxy Proxy initVals SNat expectedDat = done
+ where
+  actual =
+    xpmCdcSyncRstWith
+      @stages
+      (XpmCdcSyncRstConfig (SNat @stages) initVals)
+      clkA clkB (randomRstSrc clkA)
+
+  done =
+    outputVerifierWith
+      (\clk rst -> assertBitVector clk rst $(lift $ "outputVerifier (seed:" <> show randomSeed <> ")"))
+      clkB clkB noReset
+      expectedDat
+      (pack <$> unsafeToActiveHigh actual)
+  rstA = randomRstSrc clkA
+  rstB = unsafeFromActiveHigh $ unsafeSynchronizer clkA clkB $ unsafeToActiveHigh rstA
+
+  -- Testbench clocks
+  clkA :: Clock a
+  clkA = tbClockGen (not <$> unsafeSynchronizer clkB clkA done)
+  clkB :: Clock b
+  clkB = tbClockGen (not <$> done)
+
+expected ::
+  forall a b stages samples .
+  ( KnownDomain a
+  , KnownDomain b
+  , 2 <= stages, stages <= 10
+  ) =>
+  Proxy a ->
+  Proxy b ->
+  -- | Initial values
+  Maybe Asserted ->
+  SNat stages ->
+  SNat samples ->
+  ExpQ
+expected Proxy Proxy initVals SNat SNat = listToVecTH out1
+ where
+  out0 = unsafeToActiveHigh $
+    xpmCdcSyncRstWith
+      @stages
+      (XpmCdcSyncRstConfig (SNat @stages) initVals)
+      clkA
+      clkB
+      (randomRstSrc clkA)
+  clkA = clockGen @a
+  clkB = clockGen @b
+
+  out1 = pack <$> sampleN (natToNum @samples) out0