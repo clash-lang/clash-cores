ljw8285/clash-compiler

Selectively use tools with flags
It is now possible to selectively enable tests which use a
specific vendor tool by passing a flag to the testsuite driver,
e.g. --verilator for running tests with verilator. If a tool is not
given it's tests pass (as the tasty API does not allow deciding to
skip a test inside IsTest.run). If the tool is not found the error
is not tidied up instead of showing the contents of stderr.

This is a somewhat crude first attempt at addressing #2012.

tests/src/Test/Tasty/Iverilog.hs
@@ -6,14 +6,28 @@ module Test.Tasty.Iverilog where
 
 import           Control.Monad             (forM_)
 import           Data.Coerce               (coerce)
+import           Data.Proxy
+import           Data.Tagged
 import qualified Data.Text                 as T
 import           System.Directory          (listDirectory, copyFile)
 import           System.FilePath           ((</>))
 import           System.FilePath.Glob      (glob)
 
 import           Test.Tasty.Common
+import           Test.Tasty.Options
 import           Test.Tasty.Program
 import           Test.Tasty.Providers
+
+-- | @--iverilog@ flag for enabling tests that use iverilog.
+newtype Iverilog = Iverilog Bool
+  deriving (Eq, Ord)
+
+instance IsOption Iverilog where
+  defaultValue = Iverilog False
+  parseValue = fmap Iverilog . safeReadBool
+  optionName = pure "iverilog"
+  optionHelp = pure "Use iverilog for tests"
+  optionCLParser = flagCLParser Nothing (Iverilog True)
 
 -- | Make executable from Verilog produced by Clash using Icarus Verilog.
 --
@@ -34,11 +48,15 @@ data IVerilogMakeTest = IVerilogMakeTest
   }
 
 instance IsTest IVerilogMakeTest where
-  run optionSet IVerilogMakeTest{ivmSourceDirectory,ivmTop} progressCallback = do
-    src <- ivmSourceDirectory
-    libs <- listDirectory src
-    verilogFiles <- glob (src </> "*" </> "*.v")
-    runIcarus src (mkArgs libs verilogFiles ivmTop)
+  run optionSet IVerilogMakeTest{ivmSourceDirectory,ivmTop} progressCallback
+    | Iverilog True <- lookupOption optionSet = do
+        src <- ivmSourceDirectory
+        libs <- listDirectory src
+        verilogFiles <- glob (src </> "*" </> "*.v")
+        runIcarus src (mkArgs libs verilogFiles ivmTop)
+
+    | otherwise =
+        pure (testPassed "Ignoring test: Use --iverilog to run")
    where
     mkArgs libs files top =
          concat [["-I", l] | l <- libs]
@@ -48,7 +66,8 @@ instance IsTest IVerilogMakeTest where
     icarus workDir args = TestProgram "iverilog" args NoGlob PrintNeither False (Just workDir)
     runIcarus workDir args = run optionSet (icarus workDir args) progressCallback
 
-  testOptions = coerce (testOptions @TestProgram)
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @Iverilog)])
 
 -- | Run executable produced by 'IverilogMakeTest'.
 --
@@ -70,18 +89,22 @@ data IVerilogSimTest = IVerilogSimTest
   }
 
 instance IsTest IVerilogSimTest where
-  run optionSet IVerilogSimTest{..} progressCallback = do
-    src <- ivsSourceDirectory
+  run optionSet IVerilogSimTest{..} progressCallback
+    | Iverilog True <- lookupOption optionSet = do
+        src <- ivsSourceDirectory
 
-    -- See Note [copy data files hack]
-    lists <- glob (src </> "*/memory.list")
-    forM_ lists $ \memFile ->
-      copyFile memFile (src </> "memory.list")
+        -- See Note [copy data files hack]
+        lists <- glob (src </> "*/memory.list")
+        forM_ lists $ \memFile ->
+          copyFile memFile (src </> "memory.list")
 
-    let topExe = ivsTop <> ".exe"
-    case ivsExpectFailure of
-      Nothing -> run optionSet (vvp src [topExe]) progressCallback
-      Just exit -> run optionSet (failingVvp src [topExe] exit) progressCallback
+        let topExe = ivsTop <> ".exe"
+        case ivsExpectFailure of
+          Nothing -> run optionSet (vvp src [topExe]) progressCallback
+          Just exit -> run optionSet (failingVvp src [topExe] exit) progressCallback
+
+    | otherwise =
+        pure (testPassed "Ignoring test: Use --iverilog to run")
    where
     vvp workDir args =
       TestProgram "vvp" args NoGlob PrintNeither ivsStdoutNonEmptyFail (Just workDir)
@@ -91,4 +114,5 @@ instance IsTest IVerilogSimTest where
         (testExitCode testExit) "vvp" args NoGlob PrintNeither False
         (specificExitCode testExit) (ExpectEither expectedErr) (Just workDir)
 
-  testOptions = coerce (testOptions @TestProgram)
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @Iverilog)])

tests/src/Test/Tasty/Modelsim.hs
@@ -7,14 +7,28 @@ module Test.Tasty.Modelsim where
 import           Control.Monad             (forM_)
 import           Data.Coerce               (coerce)
 import qualified Data.List                 as List
+import           Data.Proxy
+import           Data.Tagged
 import qualified Data.Text                 as T
 import           System.Directory          (copyFile)
 import           System.FilePath           ((</>))
 import           System.FilePath.Glob      (glob)
 
 import           Test.Tasty.Common
+import           Test.Tasty.Options
 import           Test.Tasty.Program
 import           Test.Tasty.Providers
+
+-- | @--modelsim@ flag for enabling tests that use modelsim.
+newtype ModelSim = ModelSim Bool
+  deriving (Eq, Ord)
+
+instance IsOption ModelSim where
+  defaultValue = ModelSim False
+  parseValue = fmap ModelSim . safeReadBool
+  optionName = pure "modelsim"
+  optionHelp = pure "Use modelsim for tests"
+  optionCLParser = flagCLParser Nothing (ModelSim True)
 
 data ModelsimVlibTest = ModelsimVlibTest
   { mvtSourceDirectory :: IO FilePath
@@ -22,14 +36,19 @@ data ModelsimVlibTest = ModelsimVlibTest
   }
 
 instance IsTest ModelsimVlibTest where
-  run optionSet ModelsimVlibTest{mvtSourceDirectory} progressCallback = do
-    src <- mvtSourceDirectory
-    runVlib src ["work"]
+  run optionSet ModelsimVlibTest{mvtSourceDirectory} progressCallback
+    | ModelSim True <- lookupOption optionSet = do
+        src <- mvtSourceDirectory
+        runVlib src ["work"]
+
+    | otherwise =
+        pure (testPassed "Ignoring test: Use --modelsim to run")
    where
     vlib workDir args = TestProgram "vlib" args NoGlob PrintNeither False (Just workDir)
     runVlib workDir args = run optionSet (vlib workDir args) progressCallback
 
-  testOptions = coerce (testOptions @TestProgram)
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @ModelSim)])
 
 data ModelsimVlogTest = ModelsimVlogTest
   { vlogSourceDirectory :: IO FilePath
@@ -37,16 +56,21 @@ data ModelsimVlogTest = ModelsimVlogTest
   }
 
 instance IsTest ModelsimVlogTest where
-  run optionSet ModelsimVlogTest{vlogSourceDirectory} progressCallback = do
-    src <- vlogSourceDirectory
-    typeFiles <- glob (src </> "*" </> "*_types.sv")
-    allFiles <- glob (src </> "*" </> "*.sv")
-    runVlog src (["-sv", "-work", "work"] <> typeFiles <> allFiles)
+  run optionSet ModelsimVlogTest{vlogSourceDirectory} progressCallback
+    | ModelSim True <- lookupOption optionSet = do
+        src <- vlogSourceDirectory
+        typeFiles <- glob (src </> "*" </> "*_types.sv")
+        allFiles <- glob (src </> "*" </> "*.sv")
+        runVlog src (["-sv", "-work", "work"] <> typeFiles <> allFiles)
+
+    | otherwise =
+        pure (testPassed "Ignoring test: Use --modelsim to run")
    where
     vlog workDir args = TestProgram "vlog" args NoGlob PrintNeither False (Just workDir)
     runVlog workDir args = run optionSet (vlog workDir args) progressCallback
 
-  testOptions = coerce (testOptions @TestProgram)
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @ModelSim)])
 
 data ModelsimSimTest = ModelsimSimTest
   { msimExpectFailure :: Maybe (TestExitCode, T.Text)
@@ -58,19 +82,23 @@ data ModelsimSimTest = ModelsimSimTest
   }
 
 instance IsTest ModelsimSimTest where
-  run optionSet ModelsimSimTest{..} progressCallback = do
-    src <- msimSourceDirectory
+  run optionSet ModelsimSimTest{..} progressCallback
+    | ModelSim True <- lookupOption optionSet = do
+        src <- msimSourceDirectory
 
-    -- See Note [copy data files hack]
-    lists <- glob (src </> "*/memory.list")
-    forM_ lists $ \memFile ->
-      copyFile memFile (src </> "memory.list")
+        -- See Note [copy data files hack]
+        lists <- glob (src </> "*/memory.list")
+        forM_ lists $ \memFile ->
+          copyFile memFile (src </> "memory.list")
 
-    -- TODO: remove -voptargs=+acc=p for a next release of questa intel edition
-    let args = ["-voptargs=+acc=p","-batch", "-do", doScript, msimTop]
-    case msimExpectFailure of
-      Nothing -> run optionSet (vsim src args) progressCallback
-      Just exit -> run optionSet (failingVsim src args exit) progressCallback
+        -- TODO: remove -voptargs=+acc=p for a next release of questa intel edition
+        let args = ["-voptargs=+acc=p","-batch", "-do", doScript, msimTop]
+        case msimExpectFailure of
+          Nothing -> run optionSet (vsim src args) progressCallback
+          Just exit -> run optionSet (failingVsim src args exit) progressCallback
+
+    | otherwise =
+        pure (testPassed "Ignoring test: Use --modelsim to run")
    where
     vsim workDir args =
       TestProgram "vsim" args NoGlob PrintNeither False (Just workDir)
@@ -90,4 +118,5 @@ instance IsTest ModelsimSimTest where
       , "quit -code 2 -f"
       ]
 
-  testOptions = coerce (testOptions @TestProgram)
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @ModelSim)])

tests/src/Test/Tasty/SymbiYosys.hs
@@ -6,12 +6,26 @@ module Test.Tasty.SymbiYosys(SbyVerificationTest(..)) where
 
 import           Clash.Driver.Manifest     (Manifest(..), manifestFilename)
 import           Data.Coerce               (coerce)
+import           Data.Proxy
+import           Data.Tagged
 import qualified Data.Text                 as T
 import           System.FilePath
 
 import           Test.Tasty.Common
+import           Test.Tasty.Options
 import           Test.Tasty.Program
 import           Test.Tasty.Providers
+
+-- | @--symbiyosys@ flag for enabling tests that use symbiyosys.
+newtype Symbiyosys = Symbiyosys Bool
+  deriving (Eq, Ord)
+
+instance IsOption Symbiyosys where
+  defaultValue = Symbiyosys False
+  parseValue = fmap Symbiyosys . safeReadBool
+  optionName = pure "symbiyosys"
+  optionHelp = pure "Use symbiyosys for tests"
+  optionCLParser = flagCLParser Nothing (Symbiyosys True)
 
 -- | Run SymbiYosys on some Clash generated HDL
 data SbyVerificationTest = SbyVerificationTest
@@ -24,35 +38,38 @@ data SbyVerificationTest = SbyVerificationTest
   }
 
 instance IsTest SbyVerificationTest where
-  run optionSet SbyVerificationTest {..} progressCallback = do
-    src                             <- svtSourceDirectory
-    [(manifestFile, Manifest {..})] <- getManifests
-      (src </> "*" </> manifestFilename)
-    let path    = takeDirectory manifestFile
-        sbyFile = path </> T.unpack topComponent <.> "sby"
-        sourceFiles =
-          fmap (path </>)
-            . filter ((`elem` [".v", ".sv"]) . takeExtension)
-            . fmap fst
-            $ fileNames
-    writeFile sbyFile $ makeSbyFile (T.unpack topComponent) sourceFiles
-    let args    = ["-f", sbyFile]
-    let sbyTest = TestProgram "sby" args NoGlob PrintNeither False (Just src)
-    let sbyTestFail (testExit, expectedErr) = TestFailingProgram
-          (testExitCode testExit)
-          "sby"
-          args
-          NoGlob
-          PrintNeither
-          False
-          (specificExitCode testExit)
-          (ExpectEither expectedErr)
-          (Just src)
-    case svtExpectFail of
-      Nothing   -> run optionSet sbyTest progressCallback
-      Just exit -> run optionSet (sbyTestFail exit) progressCallback
+  run optionSet SbyVerificationTest {..} progressCallback
+    | Symbiyosys True <- lookupOption optionSet = do
+        src <- svtSourceDirectory
+        [(manifestFile, Manifest {..})] <- getManifests (src </> "*" </> manifestFilename)
+
+        let path    = takeDirectory manifestFile
+            sbyFile = path </> T.unpack topComponent <.> "sby"
+            sourceFiles =
+                  fmap (path </>)
+                    . filter ((`elem` [".v", ".sv"]) . takeExtension)
+                    . fmap fst
+                    $ fileNames
+
+        writeFile sbyFile $ makeSbyFile (T.unpack topComponent) sourceFiles
+
+        let args    = ["-f", sbyFile]
+        case svtExpectFail of
+          Nothing   -> run optionSet (sby src args) progressCallback
+          Just exit -> run optionSet (failingSby src args exit) progressCallback
+
+    | otherwise =
+        pure (testPassed "Ignoring test: Use --symbiyosys to run")
    where
-  testOptions = coerce (testOptions @TestProgram)
+    sby workDir args = TestProgram "sby" args NoGlob PrintNeither False (Just workDir)
+
+    failingSby workDir args (testExit, expectedErr) =
+      TestFailingProgram
+        (testExitCode testExit) "sby" args NoGlob PrintNeither False
+        (specificExitCode testExit) (ExpectEither expectedErr) (Just workDir)
+
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @Symbiyosys)])
 
 makeSbyFile :: String -> [FilePath] -> String
 makeSbyFile top files = unlines

clash-lang/clash-cores

Iverilog.hs
@@ -6,14 +6,28 @@ module Test.Tasty.Iverilog where
 
 import           Control.Monad             (forM_)
 import           Data.Coerce               (coerce)
+import           Data.Proxy
+import           Data.Tagged
 import qualified Data.Text                 as T
 import           System.Directory          (listDirectory, copyFile)
 import           System.FilePath           ((</>))
 import           System.FilePath.Glob      (glob)
 
 import           Test.Tasty.Common
+import           Test.Tasty.Options
 import           Test.Tasty.Program
 import           Test.Tasty.Providers
+
+-- | @--iverilog@ flag for enabling tests that use iverilog.
+newtype Iverilog = Iverilog Bool
+  deriving (Eq, Ord)
+
+instance IsOption Iverilog where
+  defaultValue = Iverilog True
+  parseValue = fmap Iverilog . safeReadBool
+  optionName = pure "no-iverilog"
+  optionHelp = pure "Skip iverilog tests"
+  optionCLParser = flagCLParser Nothing (Iverilog False)
 
 -- | Make executable from Verilog produced by Clash using Icarus Verilog.
 --
@@ -34,11 +48,15 @@ data IVerilogMakeTest = IVerilogMakeTest
   }
 
 instance IsTest IVerilogMakeTest where
-  run optionSet IVerilogMakeTest{ivmSourceDirectory,ivmTop} progressCallback = do
-    src <- ivmSourceDirectory
-    libs <- listDirectory src
-    verilogFiles <- glob (src </> "*" </> "*.v")
-    runIcarus src (mkArgs libs verilogFiles ivmTop)
+  run optionSet IVerilogMakeTest{ivmSourceDirectory,ivmTop} progressCallback
+    | Iverilog True <- lookupOption optionSet = do
+        src <- ivmSourceDirectory
+        libs <- listDirectory src
+        verilogFiles <- glob (src </> "*" </> "*.v")
+        runIcarus src (mkArgs libs verilogFiles ivmTop)
+
+    | otherwise =
+        pure (testPassed "Ignoring test due to --no-verilog")
    where
     mkArgs libs files top =
          concat [["-I", l] | l <- libs]
@@ -48,7 +66,8 @@ instance IsTest IVerilogMakeTest where
     icarus workDir args = TestProgram "iverilog" args NoGlob PrintNeither False (Just workDir)
     runIcarus workDir args = run optionSet (icarus workDir args) progressCallback
 
-  testOptions = coerce (testOptions @TestProgram)
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @Iverilog)])
 
 -- | Run executable produced by 'IverilogMakeTest'.
 --
@@ -70,18 +89,22 @@ data IVerilogSimTest = IVerilogSimTest
   }
 
 instance IsTest IVerilogSimTest where
-  run optionSet IVerilogSimTest{..} progressCallback = do
-    src <- ivsSourceDirectory
+  run optionSet IVerilogSimTest{..} progressCallback
+    | Iverilog True <- lookupOption optionSet = do
+        src <- ivsSourceDirectory
 
-    -- See Note [copy data files hack]
-    lists <- glob (src </> "*/memory.list")
-    forM_ lists $ \memFile ->
-      copyFile memFile (src </> "memory.list")
+        -- See Note [copy data files hack]
+        lists <- glob (src </> "*/memory.list")
+        forM_ lists $ \memFile ->
+          copyFile memFile (src </> "memory.list")
 
-    let topExe = ivsTop <> ".exe"
-    case ivsExpectFailure of
-      Nothing -> run optionSet (vvp src [topExe]) progressCallback
-      Just exit -> run optionSet (failingVvp src [topExe] exit) progressCallback
+        let topExe = ivsTop <> ".exe"
+        case ivsExpectFailure of
+          Nothing -> run optionSet (vvp src [topExe]) progressCallback
+          Just exit -> run optionSet (failingVvp src [topExe] exit) progressCallback
+
+    | otherwise =
+        pure (testPassed "Ignoring test due to --no-iverilog")
    where
     vvp workDir args =
       TestProgram "vvp" args NoGlob PrintNeither ivsStdoutNonEmptyFail (Just workDir)
@@ -91,4 +114,5 @@ instance IsTest IVerilogSimTest where
         (testExitCode testExit) "vvp" args NoGlob PrintNeither False
         (specificExitCode testExit) (ExpectEither expectedErr) (Just workDir)
 
-  testOptions = coerce (testOptions @TestProgram)
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @Iverilog)])

Modelsim.hs
@@ -7,14 +7,28 @@ module Test.Tasty.Modelsim where
 import           Control.Monad             (forM_)
 import           Data.Coerce               (coerce)
 import qualified Data.List                 as List
+import           Data.Proxy
+import           Data.Tagged
 import qualified Data.Text                 as T
 import           System.Directory          (copyFile)
 import           System.FilePath           ((</>))
 import           System.FilePath.Glob      (glob)
 
 import           Test.Tasty.Common
+import           Test.Tasty.Options
 import           Test.Tasty.Program
 import           Test.Tasty.Providers
+
+-- | @--modelsim@ flag for enabling tests that use modelsim.
+newtype ModelSim = ModelSim Bool
+  deriving (Eq, Ord)
+
+instance IsOption ModelSim where
+  defaultValue = ModelSim True
+  parseValue = fmap ModelSim . safeReadBool
+  optionName = pure "no-modelsim"
+  optionHelp = pure "Skip modelsim tests"
+  optionCLParser = flagCLParser Nothing (ModelSim False)
 
 data ModelsimVlibTest = ModelsimVlibTest
   { mvtSourceDirectory :: IO FilePath
@@ -22,14 +36,19 @@ data ModelsimVlibTest = ModelsimVlibTest
   }
 
 instance IsTest ModelsimVlibTest where
-  run optionSet ModelsimVlibTest{mvtSourceDirectory} progressCallback = do
-    src <- mvtSourceDirectory
-    runVlib src ["work"]
+  run optionSet ModelsimVlibTest{mvtSourceDirectory} progressCallback
+    | ModelSim True <- lookupOption optionSet = do
+        src <- mvtSourceDirectory
+        runVlib src ["work"]
+
+    | otherwise =
+        pure (testPassed "Ignoring test due to --no-modelsim")
    where
     vlib workDir args = TestProgram "vlib" args NoGlob PrintNeither False (Just workDir)
     runVlib workDir args = run optionSet (vlib workDir args) progressCallback
 
-  testOptions = coerce (testOptions @TestProgram)
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @ModelSim)])
 
 data ModelsimVlogTest = ModelsimVlogTest
   { vlogSourceDirectory :: IO FilePath
@@ -37,16 +56,21 @@ data ModelsimVlogTest = ModelsimVlogTest
   }
 
 instance IsTest ModelsimVlogTest where
-  run optionSet ModelsimVlogTest{vlogSourceDirectory} progressCallback = do
-    src <- vlogSourceDirectory
-    typeFiles <- glob (src </> "*" </> "*_types.sv")
-    allFiles <- glob (src </> "*" </> "*.sv")
-    runVlog src (["-sv", "-work", "work"] <> typeFiles <> allFiles)
+  run optionSet ModelsimVlogTest{vlogSourceDirectory} progressCallback
+    | ModelSim True <- lookupOption optionSet = do
+        src <- vlogSourceDirectory
+        typeFiles <- glob (src </> "*" </> "*_types.sv")
+        allFiles <- glob (src </> "*" </> "*.sv")
+        runVlog src (["-sv", "-work", "work"] <> typeFiles <> allFiles)
+
+    | otherwise =
+        pure (testPassed "Ignoring test due to --no-modelsim")
    where
     vlog workDir args = TestProgram "vlog" args NoGlob PrintNeither False (Just workDir)
     runVlog workDir args = run optionSet (vlog workDir args) progressCallback
 
-  testOptions = coerce (testOptions @TestProgram)
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @ModelSim)])
 
 data ModelsimSimTest = ModelsimSimTest
   { msimExpectFailure :: Maybe (TestExitCode, T.Text)
@@ -58,19 +82,23 @@ data ModelsimSimTest = ModelsimSimTest
   }
 
 instance IsTest ModelsimSimTest where
-  run optionSet ModelsimSimTest{..} progressCallback = do
-    src <- msimSourceDirectory
+  run optionSet ModelsimSimTest{..} progressCallback
+    | ModelSim True <- lookupOption optionSet = do
+        src <- msimSourceDirectory
 
-    -- See Note [copy data files hack]
-    lists <- glob (src </> "*/memory.list")
-    forM_ lists $ \memFile ->
-      copyFile memFile (src </> "memory.list")
+        -- See Note [copy data files hack]
+        lists <- glob (src </> "*/memory.list")
+        forM_ lists $ \memFile ->
+          copyFile memFile (src </> "memory.list")
 
-    -- TODO: remove -voptargs=+acc=p for a next release of questa intel edition
-    let args = ["-voptargs=+acc=p","-batch", "-do", doScript, msimTop]
-    case msimExpectFailure of
-      Nothing -> run optionSet (vsim src args) progressCallback
-      Just exit -> run optionSet (failingVsim src args exit) progressCallback
+        -- TODO: remove -voptargs=+acc=p for a next release of questa intel edition
+        let args = ["-voptargs=+acc=p","-batch", "-do", doScript, msimTop]
+        case msimExpectFailure of
+          Nothing -> run optionSet (vsim src args) progressCallback
+          Just exit -> run optionSet (failingVsim src args exit) progressCallback
+
+    | otherwise =
+        pure (testPassed "Ignoring test due to --no-modelsim")
    where
     vsim workDir args =
       TestProgram "vsim" args NoGlob PrintNeither False (Just workDir)
@@ -90,4 +118,5 @@ instance IsTest ModelsimSimTest where
       , "quit -code 2 -f"
       ]
 
-  testOptions = coerce (testOptions @TestProgram)
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @ModelSim)])

SymbiYosys.hs
@@ -6,12 +6,26 @@ module Test.Tasty.SymbiYosys(SbyVerificationTest(..)) where
 
 import           Clash.Driver.Manifest     (Manifest(..), manifestFilename)
 import           Data.Coerce               (coerce)
+import           Data.Proxy
+import           Data.Tagged
 import qualified Data.Text                 as T
 import           System.FilePath
 
 import           Test.Tasty.Common
+import           Test.Tasty.Options
 import           Test.Tasty.Program
 import           Test.Tasty.Providers
+
+-- | @--symbiyosys@ flag for enabling tests that use symbiyosys.
+newtype Symbiyosys = Symbiyosys Bool
+  deriving (Eq, Ord)
+
+instance IsOption Symbiyosys where
+  defaultValue = Symbiyosys True
+  parseValue = fmap Symbiyosys . safeReadBool
+  optionName = pure "no-symbiyosys"
+  optionHelp = pure "Skip symbiyosys tests"
+  optionCLParser = flagCLParser Nothing (Symbiyosys False)
 
 -- | Run SymbiYosys on some Clash generated HDL
 data SbyVerificationTest = SbyVerificationTest
@@ -24,35 +38,38 @@ data SbyVerificationTest = SbyVerificationTest
   }
 
 instance IsTest SbyVerificationTest where
-  run optionSet SbyVerificationTest {..} progressCallback = do
-    src                             <- svtSourceDirectory
-    [(manifestFile, Manifest {..})] <- getManifests
-      (src </> "*" </> manifestFilename)
-    let path    = takeDirectory manifestFile
-        sbyFile = path </> T.unpack topComponent <.> "sby"
-        sourceFiles =
-          fmap (path </>)
-            . filter ((`elem` [".v", ".sv"]) . takeExtension)
-            . fmap fst
-            $ fileNames
-    writeFile sbyFile $ makeSbyFile (T.unpack topComponent) sourceFiles
-    let args    = ["-f", sbyFile]
-    let sbyTest = TestProgram "sby" args NoGlob PrintNeither False (Just src)
-    let sbyTestFail (testExit, expectedErr) = TestFailingProgram
-          (testExitCode testExit)
-          "sby"
-          args
-          NoGlob
-          PrintNeither
-          False
-          (specificExitCode testExit)
-          (ExpectEither expectedErr)
-          (Just src)
-    case svtExpectFail of
-      Nothing   -> run optionSet sbyTest progressCallback
-      Just exit -> run optionSet (sbyTestFail exit) progressCallback
+  run optionSet SbyVerificationTest {..} progressCallback
+    | Symbiyosys True <- lookupOption optionSet = do
+        src <- svtSourceDirectory
+        [(manifestFile, Manifest {..})] <- getManifests (src </> "*" </> manifestFilename)
+
+        let path    = takeDirectory manifestFile
+            sbyFile = path </> T.unpack topComponent <.> "sby"
+            sourceFiles =
+                  fmap (path </>)
+                    . filter ((`elem` [".v", ".sv"]) . takeExtension)
+                    . fmap fst
+                    $ fileNames
+
+        writeFile sbyFile $ makeSbyFile (T.unpack topComponent) sourceFiles
+
+        let args    = ["-f", sbyFile]
+        case svtExpectFail of
+          Nothing   -> run optionSet (sby src args) progressCallback
+          Just exit -> run optionSet (failingSby src args exit) progressCallback
+
+    | otherwise =
+        pure (testPassed "Ignoring test due to --no-symbiyosys")
    where
-  testOptions = coerce (testOptions @TestProgram)
+    sby workDir args = TestProgram "sby" args NoGlob PrintNeither False (Just workDir)
+
+    failingSby workDir args (testExit, expectedErr) =
+      TestFailingProgram
+        (testExitCode testExit) "sby" args NoGlob PrintNeither False
+        (specificExitCode testExit) (ExpectEither expectedErr) (Just workDir)
+
+  testOptions =
+    coerce (coerce (testOptions @TestProgram) <> [Option (Proxy @Symbiyosys)])
 
 makeSbyFile :: String -> [FilePath] -> String
 makeSbyFile top files = unlines