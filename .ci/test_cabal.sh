712006394bfec87a16441a8462cb0d06c44c7fb9
t-wallet <tw.personal@tutanota.com>
2024-07-30 16:38:35 +0200

    Basic CI setup

---
+++ .ci/test_cabal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -xeou pipefail
+
+cabal v2-build all -fci
+cabal v2-run unittests -fci --enable-tests
+cabal v2-run doctests -fci --enable-tests
+cabal v2-run clash-testsuite -fci --enable-tests
+cabal v2-sdist
