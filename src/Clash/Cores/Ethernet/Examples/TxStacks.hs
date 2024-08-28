0c59cffccef081e4d6404839495411298ee53c21
t-wallet <tw.personal@tutanota.com>
2024-08-28 15:57:39 +0200

    Add Ethernet core
    Co-authored-by: Jasmijn Bookelmann <bookelmannjasmijn@gmail.com>
    Co-authored-by: Cato van Ojen <Baublesaurus@users.noreply.github.com>
    Co-authored-by: MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
    Co-authored-by: Jasper Laumen <96011116+JLaumen@users.noreply.github.com>
    Co-authored-by: Mart Koster <30956441+Akribes@users.noreply.github.com>
    Co-authored-by: Bryan Rinders <bryan__ajax@hotmail.com>
    Co-authored-by: Daan Weessies <daan.weessies@gmail.com>
    Co-authored-by: Rowan Goemans <goemansrowan@gmail.com>

---
+++ src/Clash/Cores/Ethernet/Examples/TxStacks.hs
@@ -0,0 +1,68 @@
+{-# language FlexibleContexts #-}
+
+{-|
+Module      : Clash.Cores.Ethernet.Examples.TxStacks
+Description : Provides the entire transmit stack as a circuit.
+-}
+module Clash.Cores.Ethernet.Examples.TxStacks
+  ( macTxStack
+  , ipTxStack
+  ) where
+
+import Clash.Cores.Crc
+import Clash.Cores.Crc.Catalog
+import Clash.Prelude
+
+import Protocols ( Circuit, (|>) )
+import Protocols.PacketStream
+
+import Clash.Cores.Ethernet.IP.IPPacketizers
+import Clash.Cores.Ethernet.IP.IPv4Types
+import Clash.Cores.Ethernet.Mac.EthernetTypes
+import Clash.Cores.Ethernet.Mac.FrameCheckSequence ( fcsInserterC )
+import Clash.Cores.Ethernet.Mac.InterpacketGapInserter ( interpacketGapInserterC )
+import Clash.Cores.Ethernet.Mac.MacPacketizers ( macPacketizerC )
+import Clash.Cores.Ethernet.Mac.PaddingInserter ( paddingInserterC )
+import Clash.Cores.Ethernet.Mac.Preamble ( preambleInserterC )
+
+
+-- | Processes bytes to send over ethernet
+macTxStack
+  :: forall (dataWidth :: Nat) (dom :: Domain) (domEth :: Domain)
+   . ( KnownNat dataWidth
+     , 1 <= dataWidth
+     , HiddenClockResetEnable dom
+     , KnownDomain domEth
+     , HardwareCrc Crc32_ethernet 8 dataWidth
+     )
+  => Clock domEth
+  -> Reset domEth
+  -> Enable domEth
+  -> Circuit (PacketStream dom dataWidth EthernetHeader) (PacketStream domEth 1 ())
+macTxStack ethClk ethRst ethEn =
+  macPacketizerC
+  |> paddingInserterC d60
+  |> fcsInserterC
+  |> preambleInserterC
+  |> asyncFifoC d4 hasClock hasReset hasEnable ethClk ethRst ethEn
+  |> exposeClockResetEnable downConverterC ethClk ethRst ethEn
+  |> exposeClockResetEnable interpacketGapInserterC ethClk ethRst ethEn d12
+
+-- | Sends IP packets to a known mac address
+ipTxStack
+  :: forall (dataWidth :: Nat) (dom :: Domain) (domEth :: Domain)
+   . ( KnownNat dataWidth
+     , 1 <= dataWidth
+     , HiddenClockResetEnable dom
+     , KnownDomain domEth
+     , HardwareCrc Crc32_ethernet 8 dataWidth
+     )
+  => Clock domEth
+  -> Reset domEth
+  -> Enable domEth
+  -> Signal dom MacAddress
+  -> Circuit (PacketStream dom dataWidth IPv4HeaderLite) (PacketStream domEth 1 ())
+ipTxStack ethClk ethRst ethEn macAddressS =
+  ipLitePacketizerC
+  |> constToEthernetC 0x8000 (MacAddress $ 0x00 :> 0x00 :> 0x00 :> 0xff :> 0xff :> 0xff :> Nil) macAddressS
+  |> macTxStack ethClk ethRst ethEn
---
+++ src/Clash/Cores/Ethernet/IP/IPv4Types.hs
@@ -0,0 +1,132 @@
+{-# language RecordWildCards #-}
+
+{-|
+Module      : Clash.Cores.Ethernet.IP.IPv4Types
+Description : Provides various data types, aliases and constants for IPv4.
+-}
+module Clash.Cores.Ethernet.IP.IPv4Types
+  ( IPv4Address(..)
+  , IPv4Header(..)
+  , IPv4HeaderLite(..)
+  , toLite
+  , toLiteC
+  , fromLite
+  , fromLiteC
+  , ipv4Broadcast
+  ) where
+
+import Clash.Prelude
+
+import Protocols
+import Protocols.PacketStream
+
+import Control.DeepSeq ( NFData )
+import qualified Data.Bifunctor as B
+import Data.Tuple
+
+-- | IPv4 address.
+newtype IPv4Address = IPv4Address (Vec 4 (BitVector 8))
+  deriving (Generic, Show, ShowX, NFDataX, NFData, Eq, BitPack)
+
+bitCoerceMap2
+  :: forall a b
+   . BitPack a
+  => BitPack b
+  => BitSize a ~ BitSize b
+  => (a -> a -> a)
+  -> b -> b -> b
+bitCoerceMap2 f x y = bitCoerce $ f (bitCoerce x) (bitCoerce y)
+
+-- | `Bits` instance, borrowed from `BitVector`.
+instance Bits IPv4Address where
+  (.&.) = bitCoerceMap2 @(BitVector 32) (.&.)
+  (.|.) = bitCoerceMap2 @(BitVector 32) (.|.)
+  xor = bitCoerceMap2 @(BitVector 32) xor
+  complement = bitCoerceMap @(BitVector 32) complement
+  shift a n = bitCoerceMap @(BitVector 32) (`shift` n) a
+  rotate a n = bitCoerceMap @(BitVector 32) (`rotate` n) a
+  bitSize = finiteBitSize . bitCoerce @IPv4Address @(BitVector 32)
+  bitSizeMaybe = bitSizeMaybe . bitCoerce @IPv4Address @(BitVector 32)
+  isSigned = isSigned . bitCoerce @IPv4Address @(BitVector 32)
+  testBit = testBit . bitCoerce @IPv4Address @(BitVector 32)
+  bit = bitCoerce @(BitVector 32) . bit
+  popCount = popCount . bitCoerce @IPv4Address @(BitVector 32)
+
+
+-- | (Almost) full IPv4 header. Does not contain options field.
+data IPv4Header = IPv4Header
+  { _ipv4Version :: BitVector 4
+  , _ipv4Ihl :: Unsigned 4
+  , _ipv4Dscp :: BitVector 6
+  , _ipv4Ecn :: BitVector 2
+  , _ipv4Length :: Unsigned 16
+  , _ipv4Id :: BitVector 16
+  , _ipv4FlagReserved :: Bool
+  , _ipv4FlagDF :: Bool
+  , _ipv4FlagMF :: Bool
+  , _ipv4FragmentOffset :: BitVector 13
+  , _ipv4Ttl :: Unsigned 8
+  , _ipv4Protocol :: Unsigned 8
+  , _ipv4Checksum :: BitVector 16
+  , _ipv4Source :: IPv4Address
+  , _ipv4Destination :: IPv4Address
+  } deriving (Show, ShowX, Eq, Generic, BitPack, NFDataX, NFData)
+
+-- | Partial IPv4 header.
+data IPv4HeaderLite = IPv4HeaderLite
+  { _ipv4lSource :: IPv4Address
+  , _ipv4lDestination :: IPv4Address
+  , _ipv4lProtocol :: Unsigned 8
+  , _ipv4lPayloadLength :: Unsigned 16
+  } deriving (Show, ShowX, Eq, Generic, BitPack, NFDataX, NFData)
+
+toLite :: IPv4Header -> IPv4HeaderLite
+toLite IPv4Header {..} = IPv4HeaderLite
+  { _ipv4lSource = _ipv4Source
+  , _ipv4lDestination = _ipv4Destination
+  , _ipv4lProtocol = _ipv4Protocol
+  , _ipv4lPayloadLength = _ipv4Length - 20 -- We do not support IHLs other than 5
+  }
+
+-- | Shrinks IPv4 headers
+toLiteC :: Circuit (PacketStream dom n IPv4Header) (PacketStream dom n IPv4HeaderLite)
+toLiteC = Circuit (swap . unbundle . go . bundle)
+  where
+    go = fmap $ B.first $ fmap $ fmap toLite
+
+fromLite :: IPv4HeaderLite -> IPv4Header
+fromLite header = IPv4Header { _ipv4Version = 4
+                             , _ipv4Ihl = ipv4Ihl
+                             , _ipv4Dscp = 0
+                             , _ipv4Ecn = 0
+                             , _ipv4Length = _ipv4lPayloadLength header + 20
+                             , _ipv4Id = 0
+                             , _ipv4FlagReserved = False
+                             , _ipv4FlagDF = False
+                             , _ipv4FlagMF = False
+                             , _ipv4FragmentOffset = 0
+                             , _ipv4Ttl = 64
+                             , _ipv4Protocol = _ipv4lProtocol header
+                             , _ipv4Checksum = 0
+                             , _ipv4Source = _ipv4lSource header
+                             , _ipv4Destination = _ipv4lDestination header
+                             }
+  where
+    ipv4Ihl = 5
+
+-- | Produce a full IPv4 header from a lite one.
+--   Note that this does *not* compute the checksum.
+fromLiteC :: Circuit (PacketStream dom n IPv4HeaderLite) (PacketStream dom n IPv4Header)
+fromLiteC = Circuit (swap . unbundle . go . bundle)
+  where
+    go = fmap $ B.first $ fmap $ fmap fromLite
+
+-- | Computes the IPv4 broadcast address.
+ipv4Broadcast
+  :: IPv4Address
+  -- ^ Host address
+  -> IPv4Address
+  -- ^ Subnet mask
+  -> IPv4Address
+  -- ^ Broadcast address
+ipv4Broadcast address subnet = address .|. complement subnet
