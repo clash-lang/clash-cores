7230b480d0738dab8a21b2d8fb7bb9a9a7ac03c2
t-wallet <tw.personal@tutanota.com>
2024-08-28 15:57:39 +0200

    Add Ethernet core
    Co-authored-by: Jasmijn Bookelmann <bookelmannjasmijn@gmail.com>
    Co-authored-by: Cato van Ojen <Baublesaurus@users.noreply.github.com>
    Co-authored-by: MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
    Co-authored-by: Jasper Laumen <96011116+JLaumen@users.noreply.github.com>
    Co-authored-by: Mart Koster <30956441+Akribes@users.noreply.github.com>
    Co-authored-by: Bryan Rinders <bryan__ajax@hotmail.com>
    Co-authored-by: Daan Weessies <daan.weessies@gmail.com>
    Co-authored-by: Rowan Goemans <goemansrowan@gmail.com>

--- test/unit-tests.hs
+++ test/unit-tests.hs
@@ -15,6 +15,7 @@ import Test.Tasty
 
 import qualified Test.Cores.Crc
 import qualified Test.Cores.Etherbone
+import qualified Test.Cores.Ethernet
 import qualified Test.Cores.LineCoding8b10b
 #if MIN_VERSION_clash_prelude(1,9,0)
 import qualified Test.Cores.Sgmii.AutoNeg
@@ -34,6 +35,7 @@ tests :: TestTree
 tests = testGroup "Unittests" $
   [ Test.Cores.Crc.tests
   , Test.Cores.Etherbone.tests
+  , Test.Cores.Ethernet.tests
   , Test.Cores.LineCoding8b10b.tests
   , Test.Cores.SPI.tests
   , Test.Cores.SPI.MultiSlave.tests
