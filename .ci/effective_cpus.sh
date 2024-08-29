6fb93926b5bb20c5d7a425a997f68bbc4de6a26a
Peter Lebbing <peter@digitalbrains.com>
2024-08-29 17:01:46 +0200

    Run CI on GitLab

---
+++ .ci/effective_cpus.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# `nproc` doesn't account for limits set by cgroups/docker. This script tries
+# to determine the effective number of cpus we can use by inspecting the shares
+# it has been given.
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ -f /sys/fs/cgroup/cpu/cpu.cfs_quota_us ]; then
+  # Older kernels (<= Ubuntu 20.04)
+  cfs_quota_us=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
+  cfs_period_us=$(cat /sys/fs/cgroup/cpu/cpu.cfs_period_us)
+elif [ -f /sys/fs/cgroup/cpu.max ]; then
+  # Newer kernels (>= Ubuntu 22.04)
+  cfs_quota_us=$(cat /sys/fs/cgroup/cpu.max | awk '{ print $1 }')
+  cfs_period_us=$(cat /sys/fs/cgroup/cpu.max | awk '{ print $2 }')
+else
+  echo "Could not determine number of effective CPUs" >&2
+  exit 1
+fi
+
+if [[ ${cfs_quota_us} == @(-1|max) ]]; then
+  # No limits set
+  nproc
+else
+  expr "${cfs_quota_us}" / "${cfs_period_us}"
+fi
