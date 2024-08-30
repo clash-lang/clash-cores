98bcebe6fea56e9930abfc2392766e7dbe4801f4
Peter Lebbing <peter@digitalbrains.com>
2024-08-30 16:46:42 +0200

    Release and master

--- .ci/report_status.sh
+++ .ci/report_status.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-commit_status=${1}
+
+set -u
+
+clash_branch=${1}
+commit_status=${2}
 username="clash-lang"
 repo="clash-cores"
 
 curl --url "https://api.github.com/repos/${username}/${repo}/statuses/${CI_COMMIT_SHA}" \
      --header 'Content-Type: application/json' \
      --header "authorization: Bearer ${GITHUB_STATUS_TOKEN}" \
-     --data "{ \"state\": \"${commit_status}\", \"target_url\": \"${CI_PIPELINE_URL}\", \"description\": \"All Gitlab pipelines\", \"context\": \"ci/gitlab/gitlab.com\" }"
+     --data "{ \"state\": \"${commit_status}\", \"target_url\": \"${CI_PIPELINE_URL}\", \"description\": \"GitLab pipelines\", \"context\": \"GitLab CI with Clash ${clash_branch}\" }"
