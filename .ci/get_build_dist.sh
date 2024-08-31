1aa22e030f3253127905c91417f639d548dae632
Peter Lebbing <peter@digitalbrains.com>
2024-08-31 12:08:45 +0200

    Split clash-cores-test-suite, configure CI

---
+++ .ci/get_build_dist.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+PLAN_JSON=${GIT_ROOT}/dist-newstyle/cache/plan.json
+
+CABAL_HOME=${HOME}/.cabal
+GHC_VERSION=$(ghc --version | grep -E -o '[0-9.]+$')
+if ghc --info | grep -q "Project Unit Id"; then
+  GHC_ABI=$(ghc --info | grep "Project Unit Id" | tail -c 7 | cut -c 1-4)
+  PKG_STORE_DIR=${CABAL_HOME}/store/ghc-${GHC_VERSION}-${GHC_ABI}
+else
+  PKG_STORE_DIR=${CABAL_HOME}/store/ghc-${GHC_VERSION}
+fi
+
+# Extract packages used from install plan
+pkgs=$(jq -r '."install-plan"[].id' "${PLAN_JSON}")
+
+# Check which exists (filters global packages)
+for pkg in ${pkgs}; do
+  if [[ -d "${PKG_STORE_DIR}/${pkg}" ]]; then
+    echo "${PKG_STORE_DIR}/${pkg}"
+    conf_file="${PKG_STORE_DIR}/package.db/${pkg}.conf"
+    if [[ -f "${conf_file}" ]]; then
+      echo "${conf_file}"
+    fi
+  fi
+done
+
+echo "${GIT_ROOT}"/dist-newstyle
+ls "${GIT_ROOT}"/.ghc.environment.*
+
+# Include symlinks to built binaries
+echo "${GIT_ROOT}"/bin
---
+++ .ci/setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -xou pipefail
+
+grep -E ' $' -n -r . --include=*.{hs,hs-boot,sh} --exclude-dir=dist-newstyle
+if [[ $? == 0 ]]; then
+    echo "EOL whitespace detected. See ^"
+    exit 1;
+fi
+
+set -e
+
+# Print out versions for debugging purposes
+cabal --version
+ghc --version
+
+# This might happen during tags on GitLab CI
+CI_COMMIT_BRANCH=${CI_COMMIT_BRANCH:-no_branch_set_by_ci}
+
+cabal v2-update | tee cabal_update_output
+
+# File may exist as part of a dist.tar.zst
+if [ ! -f cabal.project.local ]; then
+  cp .ci/cabal.project.local .
+
+  set +u
+  if [[ "$WORKAROUND_GHC_MMAP_CRASH" == "yes" ]]; then
+    sed -i 's/-workaround-ghc-mmap-crash/+workaround-ghc-mmap-crash/g' cabal.project.local
+  fi
+
+  # Fix index-state to prevent rebuilds if Hackage changes between build -> test.
+  # Note we can't simply set it to a timestamp of "now", as Cabal will error out
+  # when its index state is older than what's mentioned in cabal.project(.local).
+  most_recent_index_state=$(grep "The index-state is set to" cabal_update_output | grep -E -o '[^ ]+Z\.$' | tr -d .)
+  sed -i "s/HEAD/${most_recent_index_state}/g" cabal.project.local
+fi
+
+cat cabal.project.local
+
+rm -f ${HOME}/.cabal/config
+cabal user-config init
+sed -i "s/-- ghc-options:/ghc-options: -j$THREADS/g" ${HOME}/.cabal/config
+set +u
+if [[ "$WORKAROUND_GHC_MMAP_CRASH" == "yes" ]]; then
+  sed -i "s/ghc-options:/ghc-options: +RTS -xm20000000 -RTS -with-rtsopts=-xm20000000/g" ${HOME}/.cabal/config
+fi
+set -u
+sed -i "s/^[- ]*jobs:.*/jobs: $CABAL_JOBS/g" ${HOME}/.cabal/config
+sed -i "/remote-repo-cache:.*/d" ${HOME}/.cabal/config
+cat ${HOME}/.cabal/config
