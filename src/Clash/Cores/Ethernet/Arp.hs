4be380275a89804e961b2926ab9e26cca9b997cc
t-wallet <tw.personal@tutanota.com>
2024-09-21 16:15:42 +0200

    ARP documentation improvements

--- src/Clash/Cores/Ethernet/Arp.hs
+++ src/Clash/Cores/Ethernet/Arp.hs
@@ -35,21 +35,20 @@ import Clash.Cores.Ethernet.Mac.EthernetTypes
 
 {- |
 A fully functional ARP stack which handles ARP lookups from client circuits.
-Maintains a single-entry ARP table which the client circuit can query via the
-`ArpLookup` input. If the client-supplied IPv4 address is not found in the table,
-it transmits an ARP request for this specific address. The circuit will assert
+Maintains a multi-entry ARP table which the client circuit can query via the
+'ArpLookup' input. If the client-supplied IPv4 address is not found in the table,
+it broadcasts an ARP request for this specific address. The circuit will assert
 backpressure until either a reply has been received, or a timeout occurs. The
-maximum number of seconds the stack will wait for a reply to this request is
+maximum number of milliseconds the stack will wait for a reply to this request is
 configurable. The timeout (in seconds) of ARP table entries is configurable as well.
-All timeouts may be up to a second inaccurate.
 
-Moreover, it takes in an Ethernet stream with the ARP
-etherType (0x0806), and updates the ARP table upon receiving a valid ARP
-reply or gratuitous ARP packet.
+Moreover, it takes in an Ethernet packet stream with the ARP etherType (0x0806),
+and updates the ARP table upon receiving a valid ARP reply or gratuitous ARP packet.
 If an ARP request directed to our IPv4 address is received, it transmits a reply.
 Outbound requests receive priority over outbound replies in the output stream.
 
-__NB__: does not support Proxy ARP.
+For more specific information, refer to the documentation of the individual
+components.
 -}
 arpC ::
   forall
