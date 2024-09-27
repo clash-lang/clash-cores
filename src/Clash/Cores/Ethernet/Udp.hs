c70dc3b2cc42d6d51c9c725db6da6bcaf2a9bb76
t-wallet <tw.personal@tutanota.com>
2024-09-27 15:01:52 +0200

    Udp: add port swapping, improve docs

--- src/Clash/Cores/Ethernet/Udp.hs
+++ src/Clash/Cores/Ethernet/Udp.hs
@@ -1,34 +1,54 @@
-{-# language RecordWildCards #-}
+{-# LANGUAGE RecordWildCards #-}
 
-{-|
-Module      : Clash.Cores.Ethernet.Udp
-Description : Circuits and data types to handle the UDP protocol
+{- |
+Copyright   :  (C) 2024, QBayLogic B.V.
+License     :  BSD2 (see the file LICENSE)
+Maintainer  :  QBayLogic B.V. <devops@qbaylogic.com>
+
+Provides circuits and data types to handle the User Datagram Protocol (UDP)
+over IPv4, as specified in
+[IETF RFC 768](https://datatracker.ietf.org/doc/html/rfc768).
 -}
-module Clash.Cores.Ethernet.Udp
-  ( UdpHeaderLite(..)
-  , udpDepacketizerC
-  , udpPacketizerC
-  )
-  where
+module Clash.Cores.Ethernet.Udp (
+  -- * Data types
+  UdpHeader (..),
+  UdpHeaderLite (..),
+
+  -- * Port swapping
+  swapPorts,
+  swapPortsL,
+
+  -- * (De)packetization
+  udpDepacketizerC,
+  udpPacketizerC,
+) where
 
 import Clash.Cores.Ethernet.IP.IPv4Types
+
 import Clash.Prelude
+
+import Control.DeepSeq (NFData)
+
 import Protocols
 import Protocols.PacketStream
 
--- | The full UDP header
+{- |
+Full UDP header as defined in
+[IETF RFC 768](https://datatracker.ietf.org/doc/html/rfc768).
+-}
 data UdpHeader = UdpHeader
   { _udpSrcPort :: Unsigned 16
   -- ^ Source port
   , _udpDstPort :: Unsigned 16
   -- ^ Destination port
   , _udpLength :: Unsigned 16
-  -- ^ length of header + payload
+  -- ^ Length of header + payload
   , _udpChecksum :: Unsigned 16
-  -- ^ UDP Checksum, We do not validate or generate it
-  } deriving (Generic, NFDataX, BitPack, Eq, Show, ShowX)
+  -- ^ UDP Checksum, we do not validate or generate it
+  }
+  deriving (BitPack, Eq, Generic, NFData, NFDataX, Show, ShowX)
 
--- | UDP header
+-- | UDP header without checksum.
 data UdpHeaderLite = UdpHeaderLite
   { _udplSrcPort :: Unsigned 16
   -- ^ Source port
@@ -36,53 +56,88 @@ data UdpHeaderLite = UdpHeaderLite
   -- ^ Destination port
   , _udplPayloadLength :: Unsigned 16
   -- ^ Length of payload
-  } deriving (Generic, NFDataX, BitPack, Eq, Show, ShowX)
+  }
+  deriving (BitPack, Eq, Generic, NFData, NFDataX, Show, ShowX)
 
+-- | Create a full header from a partial one, by setting the checksum to @0@.
 fromUdpLite :: UdpHeaderLite -> UdpHeader
-fromUdpLite UdpHeaderLite{..} = UdpHeader
-  { _udpSrcPort = _udplSrcPort
-  , _udpDstPort = _udplDstPort
-  , _udpLength = _udplPayloadLength + 8
-  , _udpChecksum = 0
-  }
+fromUdpLite UdpHeaderLite{..} =
+  UdpHeader
+    { _udpSrcPort = _udplSrcPort
+    , _udpDstPort = _udplDstPort
+    , _udpLength = _udplPayloadLength + 8
+    , _udpChecksum = 0
+    }
+{-# INLINE fromUdpLite #-}
 
+-- | Create a partial header from a full one, by dropping the checksum.
 toUdpLite :: UdpHeader -> UdpHeaderLite
-toUdpLite UdpHeader{..} = UdpHeaderLite
-  { _udplSrcPort = _udpSrcPort
-  , _udplDstPort = _udpDstPort
-  , _udplPayloadLength = _udpLength - 8
-  }
+toUdpLite UdpHeader{..} =
+  UdpHeaderLite
+    { _udplSrcPort = _udpSrcPort
+    , _udplDstPort = _udpDstPort
+    , _udplPayloadLength = _udpLength - 8
+    }
+{-# INLINE toUdpLite #-}
 
--- | Parses out the UDP header from an IP stream, but ignores the checksum.
--- The first element of the metadata is the source IP of incoming packets.
-udpDepacketizerC
-  :: HiddenClockResetEnable dom
-  => KnownNat n
-  => 1 <= n
-  => Circuit
-       (PacketStream dom n IPv4HeaderLite)
-       (PacketStream dom n (IPv4Address, UdpHeaderLite))
+-- | Swap the source and destination ports in a UDP lite header.
+swapPortsL :: UdpHeaderLite -> UdpHeaderLite
+swapPortsL hdr@UdpHeaderLite{..} =
+  hdr
+    { _udplSrcPort = _udplDstPort
+    , _udplDstPort = _udplSrcPort
+    }
+{-# INLINE swapPortsL #-}
+
+-- | Swap the source and destination ports in a UDP header.
+swapPorts :: UdpHeader -> UdpHeader
+swapPorts hdr@UdpHeader{..} =
+  hdr
+    { _udpSrcPort = _udpDstPort
+    , _udpDstPort = _udpSrcPort
+    }
+{-# INLINE swapPorts #-}
+
+{- |
+Parses out the full UDP header from an IPv4 stream, but immediately drops the
+checksum without validating it. The first element of the output metadata is
+the source IPv4 address of incoming packets.
+
+Inherits latency and throughput from 'depacketizerC', where @headerBytes = 8@.
+-}
+udpDepacketizerC ::
+  (HiddenClockResetEnable dom) =>
+  (KnownNat dataWidth) =>
+  (1 <= dataWidth) =>
+  Circuit
+    (PacketStream dom dataWidth IPv4HeaderLite)
+    (PacketStream dom dataWidth (IPv4Address, UdpHeaderLite))
 udpDepacketizerC = depacketizerC (\udph ipv4lh -> (_ipv4lSource ipv4lh, toUdpLite udph))
 
--- | Serializes the UDP packet to an IP stream. The first element of the metadata
--- is the destination IP for outgoing packets. No checksum is included in the UDP header.
-udpPacketizerC
-  :: HiddenClockResetEnable dom
-  => KnownNat n
-  => 1 <= n
-  => Signal dom IPv4Address
-  -- ^ Source IP address
-  -> Circuit
-      (PacketStream dom n (IPv4Address, UdpHeaderLite))
-      (PacketStream dom n IPv4HeaderLite)
+{- |
+Serializes UDP headers to an IPv4 stream. The first element of the metadata
+is the destination IP for outgoing packets. No checksum is included in the UDP header.
+
+Inherits latency and throughput from 'packetizerC', where @headerBytes = 8@.
+-}
+udpPacketizerC ::
+  (HiddenClockResetEnable dom) =>
+  (KnownNat dataWidth) =>
+  (1 <= dataWidth) =>
+  -- | Source IPv4 address
+  Signal dom IPv4Address ->
+  Circuit
+    (PacketStream dom dataWidth (IPv4Address, UdpHeaderLite))
+    (PacketStream dom dataWidth IPv4HeaderLite)
 udpPacketizerC myIp = mapMetaS (toIp <$> myIp) |> packetizerC fst snd
-  where
-    toIp srcIp (dstIp, udpLite) = (ipLite, udpHeader)
-      where
-        udpHeader = fromUdpLite udpLite
-        ipLite = IPv4HeaderLite
-                   { _ipv4lSource = srcIp
-                   , _ipv4lDestination = dstIp
-                   , _ipv4lProtocol = 0x0011
-                   , _ipv4lPayloadLength = _udpLength udpHeader
-                   }
+ where
+  toIp srcIp (dstIp, udpLite) = (ipLite, udpHeader)
+   where
+    udpHeader = fromUdpLite udpLite
+    ipLite =
+      IPv4HeaderLite
+        { _ipv4lSource = srcIp
+        , _ipv4lDestination = dstIp
+        , _ipv4lProtocol = 0x11
+        , _ipv4lPayloadLength = _udpLength udpHeader
+        }
