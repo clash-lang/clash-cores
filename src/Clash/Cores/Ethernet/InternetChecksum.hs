6b9ff79b5bfa0b84521e787c3424231821d7ee9b
t-wallet <tw.personal@tutanota.com>
2024-09-30 15:48:37 +0200

    Remove unused internet checksum functions: see new PR

--- src/Clash/Cores/Ethernet/InternetChecksum.hs
+++ src/Clash/Cores/Ethernet/InternetChecksum.hs
@@ -10,14 +10,9 @@ Functions for computing the RFC1071 internet checksum.
 module Clash.Cores.Ethernet.InternetChecksum (
   onesComplementAdd,
   internetChecksum,
-  reduceToInternetChecksum,
-  pipelinedInternetChecksum,
-  InternetChecksumLatency,
 ) where
 
 import Clash.Prelude
-import Clash.Signal.Extra ( registerN )
-import Clash.Sized.Vector.Extra ( PipelineLatency, foldPipeline )
 
 import Data.Maybe
 
@@ -67,47 +62,3 @@ internetChecksum reset inputM = checkSumWithCarry
 
     checkSumWithCarry = carry + truncated
     nextCheckSum = add <$> inp <*> checkSumWithCarry
-
--- | Computes the internetChecksum of a vector of 16 bit words. Compared to
--- internetChecksum this is quicker as you can load multiple words per cycle
-reduceToInternetChecksum ::
-  forall (dom :: Domain) (width :: Nat).
-  HiddenClockResetEnable dom
-  => 1 <= width
-  => KnownNat width
-  => Signal dom Bool
-  -- ^ Reset signal, resets the checksum to 0 the next cycle
-  -> Signal dom (Maybe (Vec width (BitVector 16)))
-  -- ^ Input data which gets added to the checksum
-  -> Signal dom (BitVector 16)
-  -- ^ Resulting checksum
-reduceToInternetChecksum reset inputM = checkSum
-  where
-    checkSum = register 0 $ mux reset 0 checksumResult
-    input = fromMaybe (repeat 0) <$> inputM
-    checksumResult = fold onesComplementAdd <$> toSum
-    toSum = (++) <$> (singleton <$> checkSum) <*> input
-
--- | Computes the internetChecksum of a vector of 16 bit words. Same as reduceToInternetChecksum
--- but with registers between each layer of the fold. Thus the critical path is shorter, but the
--- latency is higher. The latency is equal to PipelinedICLatency width.
-pipelinedInternetChecksum ::
-  forall (dom :: Domain) (width :: Nat).
-  HiddenClockResetEnable dom
-  => 1 <= width
-  => KnownNat width
-  => Signal dom Bool
-  -- ^ Reset signal, resets the checksum to 0 the next cycle
-  -> Signal dom (Maybe (Vec width (BitVector 16)))
-  -- ^ Input data which gets added to the checksum
-  -> Signal dom (BitVector 16)
-  -- ^ Resulting checksum, the latency between input and output is PipelinedICLatency width
-pipelinedInternetChecksum resetInp inputM = checkSum
-  where
-    checkSum = register 0 $ mux reset 0 checksumResult
-    input = fromMaybe (repeat 0) <$> inputM
-    checksumResult = onesComplementAdd <$> foldPipeline 0 onesComplementAdd input <*> checkSum
-    reset = registerN (SNat :: SNat (PipelineLatency width)) False resetInp
-
--- | The latency of pipelinedInternetChecksum
-type InternetChecksumLatency (n :: Nat) = CLog 2 n + 1
