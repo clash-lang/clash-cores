c7f2eec2b19bc314e78af0f1db1ea8652a7b458c
t-wallet <tw.personal@tutanota.com>
2024-11-22 11:43:28 +0100

    IPv4: add utility functions, improve documentation.
    There is now a separate type for subnet masks to avoid confusion. Also
    added several utility functions for address classification.

--- src/Clash/Cores/Ethernet/IPv4.hs
+++ src/Clash/Cores/Ethernet/IPv4.hs
@@ -8,12 +8,14 @@ Provides various components to handle the IPv4 protocol.
 module Clash.Cores.Ethernet.IPv4 (
   -- * Data types, constants and simple utilities
   module Clash.Cores.Ethernet.IP.IPv4Types,
+
   -- * Querying the ARP subsystem
   module Clash.Cores.Ethernet.IP.EthernetStream,
+
   -- * (De)packetizing IPv4 headers
   module Clash.Cores.Ethernet.IP.IPPacketizers,
 ) where
 
 import Clash.Cores.Ethernet.IP.EthernetStream
-import Clash.Cores.Ethernet.IP.IPv4Types
 import Clash.Cores.Ethernet.IP.IPPacketizers
+import Clash.Cores.Ethernet.IP.IPv4Types
