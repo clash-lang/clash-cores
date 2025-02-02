55bbfbace546ac022cf3ae0695ebcc7361f51a55
Peter Lebbing <peter@digitalbrains.com>
2025-02-02 11:57:24 +0100

    The Ethernet core does not compile with Clash 1.8

--- test/unit-tests.hs
+++ test/unit-tests.hs
@@ -14,15 +14,7 @@ import Prelude
 import Test.Tasty
 
 import qualified Test.Cores.Crc
-import qualified Test.Cores.Ethernet
 import qualified Test.Cores.LineCoding8b10b
-#if MIN_VERSION_clash_prelude(1,9,0)
-import qualified Test.Cores.Sgmii.AutoNeg
-import qualified Test.Cores.Sgmii.BitSlip
-import qualified Test.Cores.Sgmii.RateAdapt
-import qualified Test.Cores.Sgmii.Sgmii
-import qualified Test.Cores.Sgmii.Sync
-#endif
 import qualified Test.Cores.SPI
 import qualified Test.Cores.SPI.MultiSlave
 import qualified Test.Cores.UART
@@ -30,10 +22,18 @@ import qualified Test.Cores.Xilinx.BlockRam
 import qualified Test.Cores.Xilinx.DcFifo
 import qualified Test.Cores.Xilinx.DnaPortE2
 
+#if MIN_VERSION_clash_prelude(1,9,0)
+import qualified Test.Cores.Ethernet
+import qualified Test.Cores.Sgmii.AutoNeg
+import qualified Test.Cores.Sgmii.BitSlip
+import qualified Test.Cores.Sgmii.RateAdapt
+import qualified Test.Cores.Sgmii.Sgmii
+import qualified Test.Cores.Sgmii.Sync
+#endif
+
 tests :: TestTree
 tests = testGroup "Unittests" $
   [ Test.Cores.Crc.tests
-  , Test.Cores.Ethernet.tests
   , Test.Cores.LineCoding8b10b.tests
   , Test.Cores.SPI.tests
   , Test.Cores.SPI.MultiSlave.tests
@@ -46,7 +46,8 @@ tests = testGroup "Unittests" $
 tests_modern :: [TestTree]
 #if MIN_VERSION_clash_prelude(1,9,0)
 tests_modern =
-  [ Test.Cores.Sgmii.AutoNeg.tests
+  [ Test.Cores.Ethernet.tests
+  , Test.Cores.Sgmii.AutoNeg.tests
   , Test.Cores.Sgmii.BitSlip.tests
   , Test.Cores.Sgmii.RateAdapt.tests
   , Test.Cores.Sgmii.Sgmii.tests
