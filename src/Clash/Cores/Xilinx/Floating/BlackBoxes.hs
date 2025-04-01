a2fc7778d745d48e946b4c4a40f41a9adfe1432e
Martijn Bastiaan <martijn@qbaylogic.com>
2025-04-01 12:36:26 +0200

    Use `instWIthXilinxWizard` for `fromS32With`
    The old Tcl template function set the Xilinx wizard property
    `CONFIG.Has_ACLKEN` based on analysis of the `Enable` input: if Clash
    could determine the component was always enabled, the property would be
    set to `false` causing the wizard to omit the clock enable line. The new
    method with `inst` does not currently support this, and the choice was
    made to always have the wizard emit a clock enable line, which is
    functionally correct. Synthesis might even remove the clock enable,
    leading to the same optimal result, but this was not investigated, so
    the synthesis result might be suboptimal in that respect.

--- src/Clash/Cores/Xilinx/Floating/BlackBoxes.hs
+++ src/Clash/Cores/Xilinx/Floating/BlackBoxes.hs
@@ -17,7 +17,6 @@ module Clash.Cores.Xilinx.Floating.BlackBoxes
   , mulTclTF
   , divTclTF
   , fromUTclTF
-  , fromSTclTF
   , compareTclTF
   ) where
 
@@ -224,58 +223,6 @@ fromUTclTemplate bbCtx
   pure bbText
 
 fromUTclTemplate bbCtx = error ("Xilinx.Floating.fromUTclTemplate, bad bbCtx: " <> show bbCtx)
-
-fromSTclTF :: TemplateFunction
-fromSTclTF = TemplateFunction used valid fromSTclTemplate
- where
-  used = [1,4,5]
-  valid = const True
-
-fromSTclTemplate
-  :: Backend s
-  => BlackBoxContext
-  -> State s Doc
-fromSTclTemplate bbCtx
-  | [compName] <- bbQsysIncName bbCtx
-  , (Literal _ (NumLit latency), _, _) <- bbInputs bbCtx !! 1
-  , (_, Signed inpLen, _) <- bbInputs bbCtx !! 5
-  =
- let
-  tclClkEn :: String
-  tclClkEn =
-    case bbInputs bbCtx !! 4 of
-      (DataCon _ _ [Literal Nothing (BoolLit True)], _, _) -> "false"
-      _                                                    -> "true"
-
-  bbText = [__di|
-    namespace eval $tclIface {
-      variable api 1
-      variable scriptPurpose createIp
-      variable ipName {#{compName}}
-
-      proc createIp {ipName0 args} {
-        create_ip -name floating_point -vendor xilinx.com -library ip \\
-            -version 7.1 -module_name $ipName0 {*}$args
-
-        set_property -dict [list \\
-                                 CONFIG.Operation_Type Fixed_to_float \\
-                                 CONFIG.A_Precision_Type Int#{inpLen} \\
-                                 CONFIG.Flow_Control NonBlocking \\
-                                 CONFIG.Has_ACLKEN #{tclClkEn} \\
-                                 CONFIG.C_A_Exponent_Width #{inpLen} \\
-                                 CONFIG.C_A_Fraction_Width 0 \\
-                                 CONFIG.Has_RESULT_TREADY false \\
-                                 CONFIG.C_Latency #{latency} \\
-                                 CONFIG.C_Rate 1 \\
-                                 CONFIG.Maximum_Latency false \\
-                           ] [get_ips $ipName0]
-        return
-      }
-    }|]
- in
-  pure bbText
-
-fromSTclTemplate bbCtx = error ("Xilinx.Floating.fromSTclTemplate, bad bbCtx: " <> show bbCtx)
 
 compareTclTF :: TemplateFunction
 compareTclTF = TemplateFunction used valid compareTclTemplate
